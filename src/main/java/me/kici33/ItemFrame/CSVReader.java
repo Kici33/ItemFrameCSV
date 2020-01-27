384d0b54393888133c8541199d4fcbc55c915e82
Kisi 3 <minecraftkici33@gmail.com>
2020-01-27 22:50:47 +0100

    Exluded .idea folder Fixed mistypos Fixed Sign Facing Fixed Bug With CSV Read

--- src/main/java/me/kici33/ItemFrame/CSVReader.java
+++ src/main/java/me/kici33/ItemFrame/CSVReader.java
@@ -6,7 +6,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class CSVReader {
 
@@ -19,7 +18,7 @@ public class CSVReader {
                 f.createNewFile();
             BufferedReader reader = new BufferedReader(new FileReader(f));
             reader.lines().forEach(line -> {
-                ShopItemManager.addShopItem(line.replace(",,", "").split(","));
+                ShopItemManager.addShopItem(line.replace(",,", "").replaceAll("\"", "").split(","));
             });
             reader.close();
         } catch (IOException ex) {
